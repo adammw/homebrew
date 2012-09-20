67ec3a56f6b02df70e3ab16e116fa90292985261
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-09-21 00:19:28 +1000

    pulseaudio: replace formula with v2.1, use proper sdk paths

--- Library/Formula/pulseaudio.rb
+++ Library/Formula/pulseaudio.rb
@@ -1,40 +1,41 @@
 require 'formula'
 
 class Pulseaudio < Formula
-  url 'http://freedesktop.org/software/pulseaudio/releases/pulseaudio-1.1.tar.gz'
+  url 'http://freedesktop.org/software/pulseaudio/releases/pulseaudio-2.1.tar.gz'
   head 'git://anongit.freedesktop.org/pulseaudio/pulseaudio'
   homepage 'http://pulseaudio.org'
-  md5 '1b76932ca7c4b2aa992941e41ed4594b'
+  md5 '86912af7fd4f8aa67f83182c135b2a5c'
   
+  # Dependencies from http://www.freedesktop.org/wiki/Software/PulseAudio/Ports/OSX
+  depends_on 'autoconf'
+  depends_on 'automake'
   depends_on 'intltool'
-  depends_on 'gettext'
+  depends_on 'libtool'
   depends_on 'libsndfile'
   depends_on 'speex'
+  depends_on 'gdbm'
+  depends_on 'liboil'
   depends_on 'json-c'
-  depends_on 'libsamplerate'
+
 
   def install
-    args = ["--prefix=#{prefix}",
-            "--with-udev-rules-dir=#{prefix}/lib/udev/rules.d"]
-    
-    if !File.exists? "/Developer/SDKs/MacOSX10.5.sdk" then
-      # Does not seem to build correctly with 10.6 SDK (hence the and false)
-      if File.exists? "/Developer/SDKs/MacOSX10.6.sdk" and false then
-        args << "--with-mac-sysroot=/Developer/SDKs/MacOSX10.6.sdk"
-        args << "--with-mac-version-min=10.6"
-      elsif File.exists? "/Developer/SDKs/MacOSX10.7.sdk" then
-        args << "--with-mac-sysroot=/Developer/SDKs/MacOSX10.7.sdk"
-        args << "--with-mac-version-min=10.7"
-      else
-        onoe "Error! Cannot find Mac OS X SDK"
-      end
-    end 
-    
+    args = ["--disable-jack",
+            "--disable-hal",
+            "--disable-bluez",
+            "--disable-dbus",
+            "--disable-avahi",
+            "--prefix=#{prefix}",
+            "--with-udev-rules-dir=#{prefix}/lib/udev/rules.d",
+            "--with-mac-sysroot=" + MacOS.sdk_path,
+            "--with-mac-version-min=" + MacOS.version]
+
     if ARGV.build_head? then
-      system "./autogen.sh"
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
     end
     
-    system "./configure", *args
+    system "make"
     system "make install"
   end
 
@@ -42,5 +43,3 @@ class Pulseaudio < Formula
     system "pulseaudio"
   end
 end
-
-
